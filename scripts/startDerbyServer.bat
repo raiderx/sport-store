269211d9f0a0dc5bca0e52a3a801b191b6815b6b
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2012-10-23 21:57:19 +0400

    Scripts were updated

--- scripts/startDerbyServer.bat
+++ scripts/startDerbyServer.bat
@@ -1,5 +1,5 @@
+set JAVA_HOME=C:\Program Files\Java\jre7
 set DERBY_HOME=C:\Program Files\Sun\JavaDB
 set DERBY_OPTS=-Duser.language=en -Dderby.system.home=C:\Users\karpukhin\Documents\derby_db\data
-set PATH=C:\Program Files\Java\jre7\bin;%PATH%
 
 "%DERBY_HOME%\bin\startNetworkServer.bat"
--- scripts/stopDerbyServer.bat
+++ scripts/stopDerbyServer.bat
@@ -1,5 +1,5 @@
+set JAVA_HOME=C:\Program Files\Java\jre7
 set DERBY_HOME=C:\Program Files\Sun\JavaDB
 set DERBY_OPTS=-Duser.language=en
-set PATH=C:\Program Files\Java\jre7\bin;%PATH%
 
 "%DERBY_HOME%\bin\stopNetworkServer.bat"
