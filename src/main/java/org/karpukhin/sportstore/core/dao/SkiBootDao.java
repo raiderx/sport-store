151b27c43f810ed3c7574f6496ae12226149c7f0
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2012-10-23 21:55:19 +0400

    Javadoc was written

--- src/main/java/org/karpukhin/sportstore/core/dao/SkiBootDao.java
+++ src/main/java/org/karpukhin/sportstore/core/dao/SkiBootDao.java
@@ -10,13 +10,37 @@ import java.util.List;
  */
 public interface SkiBootDao {
 
+    /**
+     * Creates new ski boot with given data or throws {@link ApplicationException} if no rows were inserted
+     * @param skiBoot data of ski boot to be created
+     * @throws ApplicationException if no rows were inserted
+     */
     void createSkiBoot(SkiBoot skiBoot);
 
+    /**
+     * Updates existing ski boot by given data or throws {@link ApplicationException} if no rows were updated
+     * @param skiBoot ski boot data to be updated
+     * @throws ApplicationException if no rows were updated
+     */
     void updateSkiBoot(SkiBoot skiBoot);
 
+    /**
+     * Removes ski boot with given unique id or throws {@link ApplicationException} if no rows were removed
+     * @param id unique id of ski boot
+     * @throws ApplicationException if no rows were removed
+     */
     void removeSkiBootById(int id);
 
+    /**
+     * Returns ski boot found by given unique id or {@code null} otherwise
+     * @param id unique id of ski boot
+     * @return ski boot found by given unique id or {@code null} otherwise
+     */
     SkiBoot findSkiBootById(int id);
 
+    /**
+     * Returns entire list of ski boots
+     * @return entire list of ski boots
+     */
     List<SkiBoot> findAllSkiBoots();
 }
